718b061f10433760c9431ba138345c679a732613
mawiie <61636445+mawiie@users.noreply.github.com>
2022-03-25 22:21:18 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,6 +4,7 @@
  * @s: input string.
  * Return: the pointer to dest.
  */
+
 char *cap_string(char *s)
 {
 	int count = 0, i;
@@ -18,7 +19,7 @@ char *cap_string(char *s)
 		{
 			if (*(s + count) == sep_words[i])
 			{
-				if ((*(s + count + 1 >= 97)) && (*(s + (count + 1)) <= 122))
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
