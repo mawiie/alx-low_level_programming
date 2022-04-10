c67ae4940c6021703279ef6e6fc50a3e70771551
mawiie <wisdom.ifode@yahoo.com>
2022-04-10 10:35:31 -0700

    Made further changes to 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,10 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if s[i] != '\0'
+	if (s[i] != '\0')
 		_putchar(s[i]);
 	else
-		_putchar(\n);
-
+		_putchar('\n');
 	i++;
-	_puts_recursion(char *s);
+	_puts_recursion(s);
 }
